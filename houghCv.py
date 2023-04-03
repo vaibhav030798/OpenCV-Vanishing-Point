579dac168763d24a1086098188455a214a98b6ef
Vaibhav Kurrey <76680409+vaibhav030798@users.noreply.github.com>
2023-04-03 13:58:34 +0530

    'All files for find vanishing point in any Images'

---
+++ houghCv.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('1603176284-vanishingpoint.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 50, 150, apertureSize = 3)
+cv2.imshow('edge', edges)
+
+lines = cv2.HoughLinesP(edges, 1, np.pi/180,100,minLineLength = 1000, maxLineGap =10)
+for line in lines:
+   x1,y1,x2 , y2 = line[0]
+   cv2.line(img, (x1,y1), (x2,y2), (0,255,0),2)
+
+cv2.imshow('image', BlurGrayImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
